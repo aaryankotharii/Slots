e37e6106a8c4233f2ba7b952b7de674464df31c1
aaryankotharii <aaryan.kothari@gmail.com>
2020-03-05 21:44:59 +0530

    background added

--- Slots/ContentView.swift
+++ Slots/ContentView.swift
@@ -10,7 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello")
+        ZStack{
+            
+            Rectangle()
+                .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255))
+                .edgesIgnoringSafeArea(.all)
+            
+            Rectangle()
+            .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
+            .rotationEffect(Angle(degrees: 45))
+            .edgesIgnoringSafeArea(.all)
+
+        }
     }
 }
 
